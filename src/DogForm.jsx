630bcb8ab20c79f274341697437804f418f53c00
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-12 17:00:22 -0500

    submitted solution

--- src/DogForm.jsx
+++ src/DogForm.jsx
@@ -10,7 +10,59 @@ Here is what the logs to the console might look like:
 "Bingo" "terrier" "5"
 */
 
+function DogForm() {
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [age, setAge] = useState("");
 
-function DogForm() {}
+  const handleSetName = (event) => setName(event.target.value);
+  const handleSetBreed = (event) => setBreed(event.target.value);
+  const handleSetAge = (event) => setAge(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name, breed, age);
+    //Clear form
+    setName("");
+    setBreed("");
+    setAge("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">
+        Name:
+        <input
+          id="name"
+          type="text"
+          name="name"
+          onChange={handleSetName}
+          value={name}
+        />
+      </label>
+      <label htmlFor="breed">
+        Breedl:
+        <input
+          id="breed"
+          type="text"
+          name="breed"
+          onChange={handleSetBreed}
+          value={breed}
+        />
+      </label>
+      <label htmlFor="age">
+        Age:
+        <input
+          id="age"
+          type="string"
+          name="age"
+          onChange={handleSetAge}
+          value={age}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 export default DogForm;
